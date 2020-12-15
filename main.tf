e5b1eb2d7723a65ca658f710e909237131bb82c8
MariaKritou <49090861+MariaKritou@users.noreply.github.com>
2020-12-15 13:33:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,8 +74,8 @@ resource "azurerm_virtual_machine" "devops" {
   
  os_profile {
    computer_name  = "virtual-machine"
-   admin_username = "var.admin_username"
-   admin_password = "var.admin_password"
+   admin_username = var.admin_username
+   admin_password = var.admin_password
    }
   
   os_profile_linux_config {
